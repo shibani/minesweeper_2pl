75fca8b225c732cbd18dfea545d6e8243f2381a9
shibani <shibani@8thlight.com>
2018-05-16 20:52:09 -0400

    fixing is_won? issue

--- lib/minesweeper_2pl/game.rb
+++ lib/minesweeper_2pl/game.rb
@@ -122,14 +122,6 @@ module Minesweeper
       end
     end
 
-    def all_non_bomb_positions_are_marked?
-      board_positions.size - bomb_positions.size == board_positions.select{ |el| el == "X" }.length
-    end
-
-    def all_bomb_positions_are_flagged?
-      bomb_positions.size == board_positions.select{ |el| el.include?('F') }.length
-    end
-
     private
 
     def move_to_position(move)
@@ -171,5 +163,12 @@ module Minesweeper
       board.positions[position] = content
     end
 
+    def all_non_bomb_positions_are_marked?
+      board_positions.size - bomb_positions.size == board_positions.select{ |el| el == "X" }.length
+    end
+
+    def all_bomb_positions_are_flagged?
+      bomb_positions.size == board_positions.select{ |el| el.include?('F') }.length
+    end
   end
 end
